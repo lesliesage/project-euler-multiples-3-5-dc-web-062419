d0f78b9bbe04878c96e4a3575cab9530309c897c
lesliesage <leslie.sage@gmail.com>
2019-09-18 10:07:31 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    nums = Array (1...limit)
+    nums.select{ |num| (num % 3) == 0 || (num % 5) == 0 }
+end
+
+def sum_multiples(limit)
+    nums = collect_multiples(limit)
+    nums.inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
-# Enter your object-oriented solution here!
+class Multiples 
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        nums = Array (1...@limit)
+        nums.select{ |num| (num % 3) == 0 || (num % 5) == 0 }
+    end
+
+    def sum_multiples
+        nums = collect_multiples()
+        nums.inject(0, :+)
+    end
+end
